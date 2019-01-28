9d8ba9a15d99e26f024323bab114350788d6a2c1
dp24678 <646595307@qq.com>
2019-01-28 17:53:24 +0800

    优化

--- Project/ChatBot/chat_bot.py
+++ Project/ChatBot/chat_bot.py
@@ -1,23 +1,24 @@
-# #-*- coding:utf-8 -*-
-# import itchat
-# import time
-# import requests
+#-*- coding:utf-8 -*-
+import itchat
+import time
+import requests
 
 
 
 
-# # def get_response(msg):
-# #     apiurl = 'http://i.itpk.cn/api.php'  #//moli机器人的网址
-# #     data={
-# #         "question": msg,    #//获取到聊天的文本信息
-# #         "api_key": "9ddf52cacd0ef429d1c63bf411b9bed6",
-# #         "api_secret": "n4gxkdyckd7p"
-# #     }
-# #
-# #     r=requests.post(apiurl,data=data) # //构造网络请求
-# #     return r.text
+# def get_response(msg):
+#     apiurl = 'http://i.itpk.cn/api.php'  #//moli机器人的网址
+#     data={
+#         "question": msg,    #//获取到聊天的文本信息
+#         "api_key": "9ddf52cacd0ef429d1c63bf411b9bed6",
+#         "api_secret": "n4gxkdyckd7p"
+#     }
 #
-#
+#     r=requests.post(apiurl,data=data) # //构造网络请求
+#     return r.text
+
+
+
 # def get_response(msg):
 #     apiUrl = 'http://www.tuling123.com/openapi/api'   #改成你自己的图灵机器人的api，上图红框中的内容，不过用我的也无所谓，只是每天自动回复的消息条数有限
 #     data = {
@@ -46,69 +47,76 @@
 #     return get_response(msg['Text'])
 #
 #
+# 自动回复
+# 封装好的装饰器，当接收到的消息是Text，即文字消息
+# @itchat.msg_register(itchat.content.TEXT)
+# def text_reply(msg):
+#     # 当消息不是由自己发出的时候
+#     if not msg['FromUserName'] == "大潘":
+#         # 发送一条提示给文件助手
+#         itchat.send_msg(u"[%s]收到好友@%s 的信息：%s\n" %
+#                         (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(msg['CreateTime'])),
+#                          msg['User']['NickName'],
+#                          msg['Text']), 'filehelper')
+#         # 回复给好友
+#         return u'[自动回复]您好，我现在有事不在，一会再和您联系。\n已经收到您的的信息：%s\n' % (msg['Text'])
+#
+
+
+#
 # if __name__ == '__main__':
-#     itchat.auto_login(True)           #自动登录
-#     itchat.run()                       #启动聊天机器人
+#     itchat.auto_login(hotReload=True)           #自动登录
+#     res = itchat.search_friends('应晖')          #被注名
+#     userName = res[0]['UserName']
+#     print(userName)
+#     while True:  # 死循环发送
+#         itchat.send('哈哈', toUserName=userName)
 
 
-# itchat.auto_login()  # C:\\Users\\Administrator\\Desktop\\a.jpg
-# itchat.send_file("ss", toUserName=r"应晖")
-import time
-
-import itchat, random, codecs, requests
-import json
+ #-*- coding:utf-8 -*-
+import requests
+import itchat
 from itchat.content import *
-from json import JSONDecoder
 
-# 微软小冰
-xiaobing_url = "http://kan.msxiaobing.com/Api/ImageAnalyze/Process?service=yanzhi"
+KEY = '4fd2dfda4d494d3a971b905ce8f177a0'
 
-
-@itchat.msg_register(PICTURE)
-def picture_reply(msg):
-    msg['Text'](msg['FileName'])
-
-    print(msg['FileName'])
-
-    header = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.73 Safari/537.36',
-        'Referer': 'http://kan.msxiaobing.com/V3/Portal',
+def get_response(msg):
+    # 构造了要发送给服务器的数据
+    apiUrl = 'http://www.tuling123.com/openapi/api'
+    data = {
+        'key'    : KEY,
+        'info'   : msg,
+        'userid' : 'wechat-robot',
     }
-    url1 = 'http://kan.msxiaobing.com/Api/Image/UploadBase64'
-    url2 = 'https://kan.msxiaobing.com/Api/ImageAnalyze/Process'
-    s = requests.Session()
-    with open(msg['FileName'], 'rb') as f:
-        image_data = f.read()
-    print(image_data)
-    r = s.post(url1, data=image_data, headers=header)
-    print(r.text)
-    imgurl = 'https://mediaplatform.msxiaobing.com' + r.json()['Url']
-    # print imgurl,"0"
-    # print r.text,"1"
+    try:
+        r = requests.post(apiUrl, data=data).json() # 字典的get方法在字典没有'text'值的时候会返回None而不会抛出异常
+        return r.get('text')                        # 为了防止服务器没有正常响应导致程序异常退出，这里用try-except捕获了异常
+    except:                                        # 如果服务器没能正常交互（返回非json或无法连接），那么就会进入下面的return
+        return                                    # 将会返回一个None
 
-    sys_time = int(time.time())
-    payload = {'service': 'yanzhi',
-               'tid': '7531216b61b14d208496ee52bca9a9a8'}
-    headerss = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36',
-        'Cookie': '_ga=GA1.2.1597838376.1504599720; _gid=GA1.2.1466467655.1504599720; ai_user=sp1jt|2017-09-05T10:53:04.090Z; cpid=YDLcMF5LPDFfSlQyfUkvMs9IbjQZMiQ2XTJHMVswUTFPAA; salt=EAA803807C2E9ECD7D786D3FA9516786; ARRAffinity=3dc0ec2b3434a920266e7d4652ca9f67c3f662b5a675f83cf7467278ef043663; ai_session=sQna0|1504664570638.64|1504664570638' + str(
-            random.randint(11, 999)),
-        'Referer': 'https://kan.msxiaobing.com/ImageGame/Portal?task=yanzhi&feid=d89e6ce730dab7a2410c6dad803b5986'
-    }
+#注册获取别人发来的信息方法
+@itchat.msg_register(TEXT)
+def tuling_reply(msg): # reply:回复
+        # print(msg.User['NickName'] +":"+ msg['Text'])    #这里输出给你发微信的人的昵称和他发送的内容
+        reply = get_response(msg['Text'])         #调取图灵机器人获取回复
+        print(reply+"\n")       #打印机器人回复的消息
+        return reply
 
-    form = {
-        'MsgId': str(sys_time) + '733',
-        'CreateTime': sys_time,
-        'content[imageUrl]': imgurl
-    }
-    r = requests.post(url2, params=payload, data=form, headers=headerss)
-    print(r.json())
-    text1 = r.json()['content']['text']
-    print(text1)
-    return text1
+# @itchat.msg_register([itchat.content.TEXT], isGroupChat=True)    #群消息的处理
+# def print_content(msg):
+#     if msg.User["NickName"]=='你希望自动回复群的名字'or msg.User["NickName"]=='另外一个你希望自动回复群的名字':    #这里可以在后面加更多的or msg.User["NickName"]=='你希望自动回复群的名字'
+#         print(msg.User['NickName'] +":"+ msg['Text'])     #打印哪个群给你发了什么消息
+#         print(get_response(msg['Text'])+"\n")           #打印机器人回复的消息
+#         return get_response(msg['Text'])
+#     else:                                         #其他群聊直接忽略
+#         pass
 
-if __name__ == '__main__':
-    itchat.auto_login(True)  # 自动登录
-    itchat.run()
+itchat.auto_login(hotReload=True)
+itchat.run()
+
+
+
+
+
 
 
